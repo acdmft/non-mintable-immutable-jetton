8c751bd0630170729722521211a76458eb778de5
gilert <crankyfox@protonmail.com>
2024-07-28 19:39:07 +0200

    add tests for deploy and getJettonData functions

--- tests/MasterContract.spec.ts
+++ tests/MasterContract.spec.ts
@@ -3,12 +3,20 @@ import { Cell, toNano } from '@ton/core';
 import { MasterContract } from '../wrappers/MasterContract';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import * as dotenv from 'dotenv';
+import { buildMasterContentCell } from '../scripts/jettonContent/onChain';
+import { JettonWallet } from '@ton/ton';
+dotenv.config();
 
 describe('MasterContract', () => {
-    let code: Cell;
+    let masterContractCode: Cell;
+    let jettonWalletCode: Cell;
+    let masterContractContent: Cell;
 
     beforeAll(async () => {
-        code = await compile('MasterContract');
+        jettonWalletCode = await compile("JettonWallet");
+        masterContractCode = await compile('MasterContract');
+        
     });
 
     let blockchain: Blockchain;
@@ -17,10 +25,25 @@ describe('MasterContract', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
-        masterContract = blockchain.openContract(MasterContract.createFromConfig({}, code));
-
         deployer = await blockchain.treasury('deployer');
+
+        masterContractContent = buildMasterContentCell({
+            name: process.env.JETTON_NAME!,
+            description: process.env.JETTON_DESCRIPTION!,
+            symbol: process.env.JETTON_SYMBOL!,
+            decimals: parseInt(process.env.JETTON_PRECISION!),
+            image: process.env.JETTON_IMG_URL!
+       
+        });
+        // const deployer = await blockchain.treasury('deployer');
+
+        masterContract = blockchain.openContract(MasterContract.createFromConfig({
+            total_supply: 888,
+            admin: deployer.address!,
+            content: masterContractContent,
+            jetton_wallet: jettonWalletCode 
+        }, masterContractCode));
+
 
         const deployResult = await masterContract.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -32,8 +55,16 @@ describe('MasterContract', () => {
         });
     });
 
-    it('should deploy', async () => {
+    it('should get jetton data after contract has been deployed', async () => {
         // the check is done inside beforeEach
         // blockchain and masterContract are ready to use
+        const jetton_data = await masterContract.getJettonData();
+        expect(jetton_data).toHaveProperty("totalSupply", BigInt(parseInt(process.env.JETTON_SUPPLY!)));
+        expect(jetton_data).toHaveProperty("mintable", false);
+        expect(jetton_data.adminAddress).toEqualAddress(deployer.address);
+        expect(jetton_data.content).toEqualCell(masterContractContent);
+        console.log("content", jetton_data.content);
+        expect(jetton_data.walletCode).toEqualCell(jettonWalletCode);
     });
+
 });
