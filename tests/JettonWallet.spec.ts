6b4780e4030bea80b94474d3489fa8eb3104811e
gilert <crankyfox@protonmail.com>
2024-08-04 15:53:54 +0200

    add tests for JettonWallet sendTransfer()

--- tests/JettonWallet.spec.ts
+++ tests/JettonWallet.spec.ts
@@ -5,6 +5,7 @@ import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
 import { MasterContract } from '../wrappers/MasterContract';
 import { buildMasterContentCell } from '../scripts/jettonContent/onChain';
+import { Op } from '../wrappers/JettonConstants';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -20,15 +21,17 @@ describe('JettonWallet', () => {
 
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
+    let nonDeployer: SandboxContract<TreasuryContract>;
     let jettonContract: any; //SandboxContract<TreasuryContract>;
     let masterContract: SandboxContract<MasterContract>;
-    let jettonWalletContract: SandboxContract<JettonWallet>;
+    // let jettonWalletContract: SandboxContract<JettonWallet>;
     let minterContractDeployRes: any;
     let jettonContractDeployRes: any;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury('deployer');
+        nonDeployer = await blockchain.treasury('non-deployer');
 
         masterContractContent = buildMasterContentCell({
             name: process.env.JETTON_NAME!,
@@ -42,6 +45,7 @@ describe('JettonWallet', () => {
             MasterContract.createFromConfig(
                 {
                     total_supply: 888,
+                    mintable: -1,
                     admin: deployer.address!,
                     content: masterContractContent,
                     jetton_wallet: jettonWalletCode,
@@ -87,7 +91,7 @@ describe('JettonWallet', () => {
     });
     it ('should get jetton wallet data after contract has been deployed', async () => {
         let initialJettonBalance = await jettonContract.getJettonBalance();
-        console.log('jettonContract ', initialJettonBalance);
+        console.log('initialJettonBalance ', initialJettonBalance);
         let jwallet_data = await jettonContract.getJettoWalletData();
         // console.log('jwallet_data', jwallet_data);
         expect(jwallet_data.jetton_balance).toBe(toNano(0));
@@ -97,7 +101,40 @@ describe('JettonWallet', () => {
     });
 
     it('wallet owner should be able to send jettons', async () => {
-        // const deployer
+        const jwallet_address = await masterContract.getWalletAddress(deployer.address);
+        const deployerJettonWallet = blockchain.openContract(JettonWallet.createFromAddress(jwallet_address))
+        const mintResult = await masterContract.sendMint(
+            deployer.getSender(),
+            deployer.address,
+            BigInt(100500),
+            toNano('0.01'), // forward ton amount
+            toNano('1') // total ton amount
+        );
+        let deployerJettonBalance = await deployerJettonWallet.getJettonBalance();
+        expect(deployerJettonBalance).toBe(BigInt(100500));
+        // console.log('initial deployerJettonBalance ', deployerJettonBalance);
+        const nonDeployerJwalletAddr = await masterContract.getWalletAddress(nonDeployer.address);
+        const nonDeployerJettonWallet = await blockchain.openContract(
+            JettonWallet.createFromAddress(nonDeployerJwalletAddr)
+        )
+        const sendResult = await deployerJettonWallet.sendTransfer(
+            deployer.getSender(), 
+            toNano('0.1'), 
+            BigInt(100), // jetton_amount toNano('0.0000005'),
+            nonDeployer.address,
+            deployer.address,
+            beginCell().endCell(), // custom payload
+            toNano('0'), // forward ton amount
+            beginCell().endCell() // forward payload
+        )
+        expect(sendResult.transactions).toHaveTransaction({ //excesses
+            to: deployerJettonWallet.address,
+            op: Op.transfer,
+            from: deployer.address,
+            success: true 
+        });
+        deployerJettonBalance = await deployerJettonWallet.getJettonBalance();
+        // console.log('new jettonWalletBalance ', deployerJettonBalance);
     })
 
 });
